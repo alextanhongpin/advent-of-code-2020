449cc92e36561054d53b5321864db17020e97b31
alextanhongpin <alextan220990@gmail.com>
2020-12-08 11:53:04 +0800

    refactor: destructure tuple into arguments

--- main_02.py
+++ main_02.py
@@ -11,23 +11,17 @@ def part_one():
   def valid_password(password, char, min, max):
     return min <= Counter(password)[char] <= max
 
-  valid = 0
   with open('input_02.txt') as f:
-    for line in f:
-      password, char, min, max = parse_line(line)
-      valid += valid_password(password, char, min, max)
-  print(valid)
+    count = sum([valid_password(*parse_line(line)) for line in f])
+    print(count) # 465
 
 def part_two():
   def valid_password(password, char, min, max):
     return (password[min-1] == char) != (password[max-1] == char)
 
-  valid = 0
   with open('input_02.txt') as f:
-    for line in f:
-      password, char, min, max = parse_line(line)
-      valid += valid_password(password, char, min, max)
-  print(valid)
+    count = sum([valid_password(*parse_line(line)) for line in f])
+    print(count) # 294
 
 def main():
   part_one()
