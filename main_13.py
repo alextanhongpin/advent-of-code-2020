f5dfdc285a85572e52caf8abef020f0e26b7f588
alextanhongpin <alextan220990@gmail.com>
2020-12-14 21:52:04 +0800

    chore: add solution 13

--- main_13.py
+++ main_13.py
@@ -1,8 +1,13 @@
+from collections import defaultdict 
 
-def schedule(input):
+def parse_input(input):
   input = [line.strip() for line in input]
   ts, bus_ids = int(input[0]), input[1]
   bus_ids = [int(bus_id) for bus_id in bus_ids.split(',') if bus_id != 'x']
+  return ts, bus_ids
+
+def schedule(input):
+  ts, bus_ids = parse_input(input)
   result = []
 
   def check_earliest_ride(n, bus_id):
@@ -16,12 +21,31 @@ def schedule(input):
   earliest_bus = bus_ids[result.index(min(result))]
   return (min(result) - ts) * earliest_bus
 
+def schedule2(input):
+  input = [line.strip() for line in input]
+  bus_ids = input[1].split(',')
+  bus_ids = [(idx, int(bus_id)) for idx, bus_id in enumerate(bus_ids) if bus_id != 'x']
+
+  t, step = bus_ids[0][0], bus_ids[0][1]
+  for delta, bus_id in bus_ids[1:]:
+    while ((t + delta) % bus_id) != 0:
+      t += step
+    step *= bus_id
+  return t
+
+
+
 def part_one():
   with open('input_13.txt') as f:
-    print(schedule(f))
+    print(schedule(f)) # 171
+
+def part_two():
+  with open('input_13.txt') as f:
+    print(schedule2(f)) # 539746751134958
 
 def main():
   part_one()
+  part_two()
 
 if __name__ == '__main__':
   main()
--- main_13_test.py
+++ main_13_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main_13 import schedule
+from main_13 import schedule, schedule2
 
 input = """939
 7,13,x,x,59,x,31,19""".split('\n')
@@ -8,6 +8,9 @@ input = """939
 class TestSchedule(unittest.TestCase):
   def test_schedule(self):
     self.assertEqual(schedule(input), 295)
+
+  def test_schedule2(self):
+    self.assertEqual(schedule2(input), 1068781)
 
 if __name__ == '__main__':
   unittest.main()
