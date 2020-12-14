8281df420acc39f3f03a2a84ab7625e3060ec18c
alextanhongpin <alextan220990@gmail.com>
2020-12-14 23:46:15 +0800

    chore: add solution 14 part 2

--- main_14.py
+++ main_14.py
@@ -12,7 +12,7 @@ def binary_mask(input):
       mask = line.split(' = ')[1]
     else:
       position, value = re.findall(PATTERN, line)[0]
-      binstr = bin(int(value)).zfill(len(mask)).replace('b', '0')
+      binstr = bin(int(value))[2:].zfill(len(mask))
 
       binval = ''
       for i in range(len(mask)):
@@ -26,13 +26,52 @@ def binary_mask(input):
 
   return sum(cache.values())
 
+def binary_mask2(input):
+  cache = {}
+
+  lines = [line.strip() for line in input]
+  for line in lines:
+    if line.startswith('mask'):
+      mask = line.strip().split(' = ')[1]
+    else:
+      position, value = re.findall(PATTERN, line)[0]
+      binstr = bin(int(position))[2:].zfill(len(mask))
+
+      binval = ''
+      for i in range(len(mask)):
+        if mask[i] == '1':
+          binval += '1'
+        elif mask[i] == '0':
+          binval += binstr[i]
+        else: 
+          binval += 'X'
+
+      combinations = binval.count('X')
+      for i in range(2**combinations):
+        combination = bin(i)[2:].zfill(combinations)
+        replaced = 0
+        cpy = list(binval)
+        for i, c in enumerate(cpy):
+          if c == 'X':
+            cpy[i] = combination[replaced]
+            replaced += 1
+        addr = int(''.join(cpy), 2)
+        cache[addr] = int(value)
+
+  return sum(cache.values())
 
 def part_one():
   with open('input_14.txt') as f:
     print(binary_mask(f)) # 6631883285184
 
+
+def part_two():
+  with open('input_14.txt') as f:
+    print(binary_mask2(f)) # 3161838538691
+
 def main():
   part_one()
+  part_two()
 
 if __name__ == '__main__':
   main()
--- main_14_test.py
+++ main_14_test.py
@@ -1,15 +1,23 @@
 import unittest
 
-from main_14 import binary_mask
+from main_14 import binary_mask, binary_mask2
 
-input = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+input1 = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0""".split('\n')
 
+input2 = """mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1""".split('\n')
+
 class TestBinaryMask(unittest.TestCase):
   def test_binary_mask(self):
-    self.assertEqual(binary_mask(input), 165)
+    self.assertEqual(binary_mask(input1), 165)
+
+  def test_binary_mask2(self):
+    self.assertEqual(binary_mask2(input2), 208)
 
 if __name__ == '__main__':
   unittest.main()
